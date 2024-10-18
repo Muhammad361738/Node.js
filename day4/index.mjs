0bce2961bf08fb309e769018ec6998e3a4712734
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-19 01:33:10 +0500

    explore the common js module and ES6 modules

---
+++ day4/index.mjs
@@ -0,0 +1,13 @@
+// use common js module in Nod ejs how we can import and export 
+
+// in ES^ you cannot use module . export function 
+
+export function simple (){
+    console.log("Simple is complex so try to understand ")
+}
+
+// use module to export 
+// module.exports = simple
+
+// Now we discuss the ES6 Modules in which you can need the mjs extension and you 
+// cannnot use the module .export function in ES6 You can use export key word  
---
+++ day4/secondModule.mjs
@@ -0,0 +1,6 @@
+// const simple = require("./index")
+// in ES^ module you can not use require function 
+// use import 
+// and add additional thiings which is type : module add in oakage.jasn file 
+import {simple} from "./index.mjs"
+simple()
